5cb0074b68da20dc482aba8a5d540af2226f4585
Sam Shervin S <121488288+sam-shervin@users.noreply.github.com>
2024-03-07 09:07:15 +0530

    Time: 65 ms (35.10%), Space: 34.4 MB (41.65%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
@@ -4,11 +4,6 @@ public:
         // Map rule keys to their corresponding indices
         unordered_map<string, int> ruleIndices = {{"type", 0}, {"color", 1}, {"name", 2}};
         
-        // Check if the ruleKey exists in the map
-        if (ruleIndices.find(ruleKey) == ruleIndices.end()) {
-            return 0; // If not found, return 0
-        }
-        
         int index = ruleIndices[ruleKey];
         int count = 0;
         
