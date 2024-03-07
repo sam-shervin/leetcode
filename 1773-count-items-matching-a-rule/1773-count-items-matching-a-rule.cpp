64ed5cae8d6375d7f007e9a17eca2a77719902f2
Sam Shervin S <121488288+sam-shervin@users.noreply.github.com>
2024-03-07 09:11:39 +0530

    Time: 60 ms (64.12%), Space: 34.4 MB (41.65%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
         unordered_map<string, int> ruleIndices = {{"type", 0}, {"color", 1}, {"name", 2}};
-        if (ruleIndices.find(ruleKey) == ruleIndices.end()) {
-            return 0;
-        }
         int index = ruleIndices[ruleKey];
         int count = 0;
         for (const auto& item : items) {
