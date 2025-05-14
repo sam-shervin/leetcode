09e143883dc03ae0f3fcad42bf632e6f87ba3f35
Sam Shervin S <121488288+sam-shervin@users.noreply.github.com>
2025-05-14 20:10:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (6.28%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = dict()
+        for i in range(len(nums)):
+            if nums[i] in hashmap.keys():
+                return [hashmap[nums[i]], i]
+            else:
+                hashmap[target-nums[i]] = i
+            
+        
