9d177065b6a7d49d1f571951c268855f7f59b30b
Sam Shervin S <121488288+sam-shervin@users.noreply.github.com>
2025-08-03 20:53:09 +0530

    Time: 1 ms (97.71%) | Memory: 18.2 MB (19.41%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -2,17 +2,11 @@ from bisect import bisect_left
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        sub = []  # stores the smallest possible tail of increasing subsequences
-
+        sub = []
         for num in nums:
-            # Find index in sub where num can replace or be appended
             idx = bisect_left(sub, num)
-
             if idx == len(sub):
-                # num is bigger than any element in sub -> append
                 sub.append(num)
             else:
-                # Replace the element at idx with num (better candidate)
                 sub[idx] = num
-
         return len(sub)
