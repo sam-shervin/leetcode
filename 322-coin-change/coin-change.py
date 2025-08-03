f557f142b71c1eec9030e6b7e766ac8712342b43
Sam Shervin S <121488288+sam-shervin@users.noreply.github.com>
2025-08-03 20:14:04 +0530

    Time: 800 ms (43.05%) | Memory: 18.1 MB (87.86%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,21 +1,14 @@
-from collections import deque
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0:
-            return 0
-
-        coins.sort(reverse=True)  # larger coins first (better pruning)
-        visited = set([0])
-        queue = deque([(0, 0)])  # (current_amount, steps)
-
-        while queue:
-            curr, steps = queue.popleft()
+        coins.sort()
+        dp = [0] * (amount+1)
+        for i in range(1, amount+1):
+            minn = float('inf')
             for c in coins:
-                nxt = curr + c
-                if nxt == amount:
-                    return steps + 1
-                if nxt < amount and nxt not in visited:
-                    visited.add(nxt)
-                    queue.append((nxt, steps + 1))
-        return -1
+                diff = i - c
+                if diff < 0:
+                    break
+                minn = min(minn, 1 + dp[diff])
+            dp[i] = minn
+        minn = dp[amount]
+        return -1 if minn == float('inf') else minn
