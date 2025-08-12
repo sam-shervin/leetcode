e621fe068e45c77df5c7bf50efc07c4c6b6f4fb4
Sam Shervin S <121488288+sam-shervin@users.noreply.github.com>
2025-08-13 00:16:27 +0530

    Time: 7 ms (50.53%) | Memory: 24.2 MB (19.79%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -4,10 +4,17 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq_map = Counter(nums)
+        n = len(nums)
         
-        heap = []
+        # Buckets: index = frequency, value = list of numbers
+        buckets = [[] for _ in range(n + 1)]
         for num, freq in freq_map.items():
-            heapq.heappush(heap, (freq, num))
-            if len(heap)>k:
-                heapq.heappop(heap)
-        return [i for _, i in heap]
+            buckets[freq].append(num)
+        
+        # Collect top k starting from highest frequency
+        res = []
+        for freq in range(n, 0, -1):
+            for num in buckets[freq]:
+                res.append(num)
+                if len(res) == k:
+                    return res
