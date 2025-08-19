5228cce3fabc205e67b087b947ec581a80607d2a
Sam Shervin S <121488288+sam-shervin@users.noreply.github.com>
2025-08-19 21:23:03 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (89.12%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -6,15 +6,16 @@
 #         self.right = right
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        idx_map = {val: i for i, val in enumerate(inorder)}
+        
         self.post_idx = len(postorder) - 1
-
         def helper(left, right):
             if left > right:
                 return None
             root_val = postorder[self.post_idx]
             self.post_idx -= 1
             root = TreeNode(root_val)
-            index = inorder.index(root_val)
+            index = idx_map[root_val]
             root.right = helper(index + 1, right)
             root.left = helper(left, index - 1)
             return root
