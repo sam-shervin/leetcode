2495ce757189a3fc4eb89cbcc0c1a50bb0e734ac
Sam Shervin S <121488288+sam-shervin@users.noreply.github.com>
2025-08-20 00:29:19 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (37.14%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,18 +1,18 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        if not numRows:
+        if numRows == 0:
             return []
         if numRows == 1:
             return [[1]]
-        else:
-            a = [[1],[1,1]]
-            i = len(a)
-            while not i==numRows:
-                a.append([])
-                a[i].append(1)
-                for x in range(1, i):
-                    a[i].append(a[i-1][x-1]+a[i-1][x])
-                a[i].append(1)
-                i+=1
-            return a
+        
+        a = [[1], [1, 1]]
+        
+        for i in range(2, numRows):
+            row = [1]
+            for j in range(1, i):
+                row.append(a[i-1][j-1] + a[i-1][j])
+            row.append(1)
+            a.append(row)
+        
+        return a
 
