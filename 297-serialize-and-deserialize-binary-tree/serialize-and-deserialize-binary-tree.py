ada2077b53ed38997801eb95f4aae1c458114fa1
Sam Shervin S <121488288+sam-shervin@users.noreply.github.com>
2025-08-19 16:39:08 +0530

    Time: 96 ms (23.71%) | Memory: 21.3 MB (57.23%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -1,45 +1,35 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
 from collections import deque
+
 class Codec:
 
     def serialize(self, root):
         if not root:
             return "n"
 
-        q = [root]   # use a simple list
+        q = [root]
         i = 0
 
-        while i < len(q):   # walk with an index
+        while i < len(q):
             node = q[i]
             if node:
                 q.append(node.left)
                 q.append(node.right)
             i += 1
 
-        # now turn it into a string
         return ",".join(str(node.val) if node else "n" for node in q)
 
-        
 
     def deserialize(self, data):
-        """Decodes your encoded data to tree."""
         if data == "n":
             return None
 
         vals = data.split(",")
         root = TreeNode(int(vals[0]))
-
-        q = [root]   # again, list + index
-        i = 0
+        q = deque([root])   # deque instead of list
         j = 1  # pointer into vals
 
-        while i < len(q) and j < len(vals):
-            node = q[i]
+        while q and j < len(vals):
+            node = q.popleft()
             if node:
                 # left child
                 if vals[j] != "n":
@@ -52,12 +42,5 @@ class Codec:
                     node.right = TreeNode(int(vals[j]))
                     q.append(node.right)
                 j += 1
-            i += 1
 
         return root
-        
-
-# Your Codec object will be instantiated and called as such:
-# ser = Codec()
-# deser = Codec()
-# ans = deser.deserialize(ser.serialize(root))
